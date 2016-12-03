1766af99419f9afa9b7e19f7ec548c5712ac4abf
Earthcomputer <burtonjae@hotmail.co.uk>
2016-12-03 11:21:23 +0000

    Simple switch to jmonkeyengine

---
+++ src/main/java/net/earthcomputer/playinggod/main/Main.java
@@ -0,0 +1,26 @@
+package net.earthcomputer.playinggod.main;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+
+public class Main extends SimpleApplication {
+
+	@Override
+	public void simpleInitApp() {
+		Box b = new Box(1, 1, 1);
+		Geometry geom = new Geometry("Box", b);
+		Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+		mat.setColor("Color", ColorRGBA.Blue);
+		geom.setMaterial(mat);
+		rootNode.attachChild(geom);
+	}
+
+	public static void main(String[] args) {
+		Main main = new Main();
+		main.start();
+	}
+
+}
